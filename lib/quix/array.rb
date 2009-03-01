d8ac7c3f64daedec2e69cd2c895be5dd3e6a4bd8
quix <quixoticsycophant@gmail.com>
2009-03-01 11:22:53 -0500

    remove odd stuff in Array

--- lib/quix/array.rb
+++ lib/quix/array.rb
@@ -1,28 +1,5 @@
 
 class Array
-  [
-   nil,
-   :second,
-   :third,
-   :fourth,
-   :fifth,
-   :sixth,
-   :seventh,
-   :eighth,
-   :ninth,
-   :tenth,
-  ].each_with_index { |name, index|
-    if name
-      define_method(name) {
-        self[index]
-      }
-    end
-  }
-
-  def penultimate
-    self[-2]
-  end
-
   alias_method :head, :first
 
   def tail
